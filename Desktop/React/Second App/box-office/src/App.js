32a1709b1f676ec094edba09a52a67de7f122147
Januka Fernando <janukashehan@gmail.com>
2021-04-20 19:23:39 +0530

    NavBar

--- Desktop/React/Second App/box-office/src/App.js
+++ Desktop/React/Second App/box-office/src/App.js
@@ -1,25 +1,34 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Navs from './components/Navs'
+import Home from './pages/Home';
+import Starred from './pages/Starred';
+
 function App() {
   return (
+    <div>
+      <Navs />
     <Switch>
       <Route exact path = "/">
-        This is homepage
+        <Home />
       </Route>
 
       <Route exact path = "/starred">
-        This is starred
+        <Starred />
       </Route>
 
       <Route>
-        This is 404 page
+        <div>
+          Not found
+        </div>
       </Route>
 
 
 
 
     </Switch>
+    </div>
   );
 }
 
