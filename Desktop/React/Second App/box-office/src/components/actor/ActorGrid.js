0142c828df6e1562a49a09331d8ea45963de7d6e
Januka Fernando <janukashehan@gmail.com>
2021-04-24 17:10:30 +0530

    styled components

--- Desktop/React/Second App/box-office/src/components/actor/ActorGrid.js
+++ Desktop/React/Second App/box-office/src/components/actor/ActorGrid.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ActorCard from './ActorCard'
-import IMAGE_NOT_FOUND from '../../images/not-found.png'
+import IMAGE_NOT_FOUND from '../../images/not-found.png';
+
+import { FlexGrid } from '../Styled';
 
 const ActorGrid = ({ data }) => {
-    return <div>{
+    return <FlexGrid>{
         data.map( ({ person }) => <ActorCard key={person.id}
         name={person.name} country={person.country ? person.country.name : null} birthday={person.birthday} deathday={person.deathday} gender={person.gender}  image={person.image ? person.image.medium : IMAGE_NOT_FOUND} summary={person.summary} />)
         }
-    </div>
+    </FlexGrid>
 }
 
 export default ActorGrid
