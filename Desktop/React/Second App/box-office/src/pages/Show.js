b801db9c928b0768c17c3e64553665df847489d4
Januka Fernando <janukashehan@gmail.com>
2021-04-26 20:33:02 +0530

    styling components

--- Desktop/React/Second App/box-office/src/pages/Show.js
+++ Desktop/React/Second App/box-office/src/pages/Show.js
@@ -7,6 +7,7 @@ import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import ShowMainData from '../components/show/ShowMainData';
 import { apiGet } from '../misc/config';
+import { InfoBlock, ShowPageWrapper } from './Show.styled';
 
 
 
@@ -69,29 +70,29 @@ const Show = () => {
     }
 
     
-    return <div>
+    return <ShowPageWrapper>
         <ShowMainData image={show.image} name={show.name} rating={show.rating} summary={show.summary} tags={show.genres}  
         />
 
-        <div>
+        <InfoBlock>
             <h2>Details</h2>
             <Details status={show.status} network={show.network} premiered={show.premiered} 
             />
-        </div>
+        </InfoBlock>
 
-        <div>
+        <InfoBlock>
             <h2>Seasons</h2>
             <Seasons seasons={show._embedded.seasons}
             />
-        </div>
+        </InfoBlock>
 
-        <div>
+        <InfoBlock>
             <h2>Cast</h2>
             <Cast cast={show._embedded.cast}
             />
-        </div>
+        </InfoBlock>
 
-    </div>
+    </ShowPageWrapper>
 }
 
 export default Show
