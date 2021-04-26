e448564b79cd3ef48861565ab1d4a1333be9588d
Januka Fernando <janukashehan@gmail.com>
2021-04-26 17:22:51 +0530

    error loadin messege

--- Desktop/React/Second App/box-office/src/pages/Show.js
+++ Desktop/React/Second App/box-office/src/pages/Show.js
@@ -3,17 +3,45 @@ import { useParams } from 'react-router-dom';
 import { apiGet } from '../misc/config';
 
 const Show = () => {
-
+ 
     const { id } = useParams();
     // eslint-disable-next-line
     const [show, setShow] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect( ()=> {
-        apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
-            setShow(results);
 
-        })
+        let isMounted = true;
+
+        apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`).then(results => {
+            
+                if (isMounted){
+                    setShow(results);
+                    setIsLoading(false);
+                }  
+            }) 
+            
+
+        .catch(err => {
+            if (isMounted){
+                setError(err.messege);
+                setIsLoading(false);
+            }
+        });
+
+        return () => {
+            isMounted = false;
+        }
     }, [id])
+
+    if(isLoading){
+        return <div>Data is being loaded</div>
+    };
+
+    if(error){
+        return <div>Error Occured : {error}</div>
+    }
     
     return (<div>This is show page</div>
     );
