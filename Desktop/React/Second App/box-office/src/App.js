813bb3069cdf9cfea23412fe93ef7e8957bdc08e
Januka Fernando <janukashehan@gmail.com>
2021-04-28 00:46:21 +0530

    desiged few style components

--- Desktop/React/Second App/box-office/src/App.js
+++ Desktop/React/Second App/box-office/src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import Home from './pages/Home';
 import Show from './pages/Show';
 import Starred from './pages/Starred';
 
+const theme = {
+  mainColors: {
+    blue: '#2400ff',
+    gray: '#c6c6c6',
+    dark: '#353535',
+  },
+};
+
+
 function App() {
   return (
+
+    <ThemeProvider theme={theme}>
 
     <Switch>
       <Route exact path = "/">
@@ -27,6 +39,8 @@ function App() {
         </div>
       </Route>
       </Switch>
+
+      </ThemeProvider>
 
   );
 }
