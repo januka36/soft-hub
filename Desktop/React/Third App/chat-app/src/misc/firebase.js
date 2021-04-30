a5504c38124efc3b8054185e8526e6be6038a4d8
Januka Fernando <janukashehan@gmail.com>
2021-05-01 02:56:21 +0530

    added firebase

---
+++ Desktop/React/Third App/chat-app/src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+        apiKey: "AIzaSyBtKrWKD_hyPz0ZuWOmY6_RJ5m3FIQiUEE",
+        authDomain: "chat-web-app-921b0.firebaseapp.com",
+        projectId: "chat-web-app-921b0",
+        storageBucket: "chat-web-app-921b0.appspot.com",
+        messagingSenderId: "292474989693",
+        appId: "1:292474989693:web:bca56b6271023b71839514"
+};
+
+const app = firebase.initializeApp(config);
