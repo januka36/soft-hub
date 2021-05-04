73044786313c413676a818a4be4353580221c2ed
Januka Fernando <janukashehan@gmail.com>
2021-05-05 04:18:45 +0530

    creater rooms context

--- Desktop/React/Third App/chat-app/src/pages/Home.js
+++ Desktop/React/Third App/chat-app/src/pages/Home.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import { Col, Grid, Row } from 'rsuite'
+import { RoomsProvider } from '../components/context/rooms.context'
 import Sidebar from '../components/Sidebar'
 
 const Home = () => {
-    return <Grid fluid className="h-100">
+    return (
+    <RoomsProvider>
+    <Grid fluid className="h-100">
         <Row className="h-100">
             <Col xs={24} md={8} className="h-100">
                 <Sidebar />
             </Col>
         </Row>
     </Grid>
-
+    </RoomsProvider>
+    );
 }
 
 export default Home
