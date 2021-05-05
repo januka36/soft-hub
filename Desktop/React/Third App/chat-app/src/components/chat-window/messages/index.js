8b9d0d8e05d26a1f67fba4a4b330d7c78083e57e
Januka Fernando <janukashehan@gmail.com>
2021-05-06 04:21:28 +0530

    created chat page

--- Desktop/React/Third App/chat-app/src/components/chat-window/messages/index.js
+++ Desktop/React/Third App/chat-app/src/components/chat-window/messages/index.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router';
+import { database } from '../../../misc/firebase';
+import { transformToArrayWIthId } from '../../../misc/helpers';
+import MessageItem from './MessageItem';
 
 const Messages = () => {
-    return (
-        <div>
-            hello
-        </div>
-    )
+    const {chatId} = useParams();
+    const [messages, setMessages] = useState(null);
+
+    const isChatEmpty = messages && messages.length === 0;
+    const canShowMessages = messages && messages.length > 0;
+
+    useEffect(()=>{
+        const messagesRef = database.ref('/messages');
+        messagesRef.orderByChild('roomId').equalTo(chatId).on('value', (snap)=>{
+            const data = transformToArrayWIthId(snap.val());
+
+            setMessages(data);
+        })
+
+        return ()=>{
+            messagesRef.off('value');
+        }
+    }, [chatId])
+
+    return <ul className="msg-list custom-scroll ">
+
+        {isChatEmpty && <li>
+            No messages yet
+            </li>}
+        {canShowMessages && messages.map(msg => <MessageItem key={msg.id} message={msg} />)}
+        </ul>
+
 }
 
 export default Messages
