551d2ce934010683046d10c6937e607403a75c1b
Januka Fernando <janukashehan@gmail.com>
2021-05-05 21:29:02 +0530

    created layout for chat page

---
+++ Desktop/React/Third App/chat-app/src/components/chat-window/messages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Messages = () => {
+    return (
+        <div>
+            hello
+        </div>
+    )
+}
+
+export default Messages
--- Desktop/React/Third App/chat-app/src/pages/Home/index.js
+++ Desktop/React/Third App/chat-app/src/pages/Home/index.js
@@ -1,24 +1,39 @@
 import React from 'react'
-import { Route, Switch } from 'react-router'
+import { Route, Switch, useRouteMatch } from 'react-router'
 import { Col, Grid, Row } from 'rsuite'
 import { RoomsProvider } from '../../components/context/rooms.context'
 import Sidebar from '../../components/Sidebar'
+import { useMediaQuery } from '../../misc/custom-hooks'
 import Chat from './Chat'
 
 const Home = () => {
+
+    const isDesktop = useMediaQuery('(min-width:992px)');
+    const { isExact } = useRouteMatch();
+
+    const canRenderSidebar = isDesktop || isExact;
+
     return (
     <RoomsProvider>
     <Grid fluid className="h-100">
         <Row className="h-100">
-            <Col xs={24} md={8} className="h-100">
-                <Sidebar />
-            </Col>
+            {canRenderSidebar &&
+                <Col xs={24} md={8} className="h-100">
+                    <Sidebar />
+                </Col>
+            }
+            
             <Switch>
                 <Route exact path="/chat/:chatId">
                     <Col xs={24} md={16} className="h-100">
                         <Chat />
                     </Col>
 
+                </Route>
+                <Route>
+                    {isDesktop && <Col xs={24} md={16} className="h-100">
+                        <h6 className="text-center mt-page">Please select chat</h6>
+                    </Col>}
                 </Route>
             </Switch>
         </Row>
