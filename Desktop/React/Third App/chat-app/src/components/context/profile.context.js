bfa853fdff1d8b0318ebf04c37cdeaad5a043dd8
Januka Fernando <janukashehan@gmail.com>
2021-05-06 05:27:22 +0530

    real time user

--- Desktop/React/Third App/chat-app/src/components/context/profile.context.js
+++ Desktop/React/Third App/chat-app/src/components/context/profile.context.js
@@ -1,5 +1,17 @@
 import React, { createContext, useContext, useEffect, useState } from 'react';
+import firebase from 'firebase/app';
 import { auth, database } from '../../misc/firebase';
+
+
+export const isOfflineForDatabase = {
+    state: 'offline',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
+
+const isOnlineForDatabase = {
+    state: 'online',
+    last_changed: firebase.database.ServerValue.TIMESTAMP,
+};
 
 
 const ProfileContext = createContext();
@@ -13,10 +25,11 @@ export const ProfileProvider = ( {children} ) => {
     useEffect(()=>{
 
         let userRef;
+        let userStatusRef;
 
         const authUnsub = auth.onAuthStateChanged( authObj => {
             if(authObj){
-
+                userStatusRef = database.ref(`/status/${authObj.uid}`);
                 userRef = database.ref(`/profiles/${authObj.uid}`);
 
                 userRef.on('value', (snap) => {
@@ -36,12 +49,30 @@ export const ProfileProvider = ( {children} ) => {
 
             });
 
+            database.ref('.info/connected').on('value', (snapshot)=> {
+                if (snapshot.val() === false) {
+                    return;
+                };
+            
+                userStatusRef.onDisconnect().set(isOfflineForDatabase).then(()=> {
+     
+                userStatusRef.set(isOnlineForDatabase);
+                });
+            });
+
 
             } else {
 
                 if(userRef){
                     userRef.off();
                 }
+
+                if(userStatusRef){
+                    userStatusRef.off()
+                }
+
+                database.ref('.info/connected').off();
+
                 setProfile(null);
                 setIsLoading(false);
             }
@@ -50,8 +81,14 @@ export const ProfileProvider = ( {children} ) => {
         return () => {
             authUnsub();
 
+            database.ref('.info/connected').off();
+
             if(userRef){
                 userRef.off();
+            }
+
+            if(userStatusRef){
+                userStatusRef.off()
             }
         }
 
