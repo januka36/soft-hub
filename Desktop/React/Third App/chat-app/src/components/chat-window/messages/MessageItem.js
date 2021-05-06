265bf0380bdfbb5c2f83541b8aef92ee64b3bd04
Januka Fernando <janukashehan@gmail.com>
2021-05-07 01:47:06 +0530

    added hover to msg item

--- Desktop/React/Third App/chat-app/src/components/chat-window/messages/MessageItem.js
+++ Desktop/React/Third App/chat-app/src/components/chat-window/messages/MessageItem.js
@@ -1,6 +1,7 @@
 import React, { memo } from 'react'
 import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
+import { useHover } from '../../../misc/custom-hooks';
 import { auth } from '../../../misc/firebase';
 import { useCurrentRoom } from '../../context/current-room.context';
 import PresenceDot from '../../PresenceDot';
@@ -11,6 +12,8 @@ const MessageItem = ({message, handleAdmin}) => {
 
     const {author, createdAt, text} = message; 
 
+    const [selfRef, isHovered] = useHover();
+
     const isAdmin = useCurrentRoom(v => v.isAdmin);
     const admins = useCurrentRoom(v => v.admins);
 
@@ -19,7 +22,7 @@ const MessageItem = ({message, handleAdmin}) => {
     const canGrantAdmin = isAdmin && !isAuthor;
 
     return (
-    <li className="padded mb-1">
+    <li className={`padded mb-1 cursor-pointer ${isHovered ? 'bg-black-06' : '' }`} ref={selfRef}>
         <div className="d-flex align-items-center font-bolder mb-1">
 
             <PresenceDot uid={author.uid}/>
