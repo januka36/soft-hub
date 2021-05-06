cd4873b906272ac568e63fdbbb6cf1a81844c495
Januka Fernando <janukashehan@gmail.com>
2021-05-06 22:33:24 +0530

    displayed real time presence

--- Desktop/React/Third App/chat-app/src/components/chat-window/messages/MessageItem.js
+++ Desktop/React/Third App/chat-app/src/components/chat-window/messages/MessageItem.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import TimeAgo from 'timeago-react';
+import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
 import ProfileInfoBtnModal from './ProfileInfoBtnModal';
 
@@ -9,6 +10,9 @@ const MessageItem = ({message}) => {
 
     return <li className="padded mb-1">
         <div className="d-flex align-items-center font-bolder mb-1">
+
+            <PresenceDot uid={author.uid}/>
+
             <ProfileAvatar src={author.avatar} name={author.name} className="ml-1" size="xs"/>
             
             <ProfileInfoBtnModal profile={author} appearance="link" className="p-0 ml-1 text-black"/>
